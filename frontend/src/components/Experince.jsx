944f8255c4ffcae9ad8e6e76a9c1cd4257e10de9
John <johnjerry8749@gmail.com>
2026-07-26 13:31:14 +0100

    Refactor Experience component for improved layout and styling

--- frontend/src/components/Experince.jsx
+++ frontend/src/components/Experince.jsx
@@ -1,11 +1,21 @@
-
-
 const Experince = () => {
   return (
-    <div className="p-2 mt-2 border border-danger">
-      <h2>Experience</h2>
-    </div>
-  )
-}
+    <div className="p-2 mt-2 ">
+      <div className=" w-auto d-flex justify-content-center gap-1 align-items-center">
+        <div
+          className="border  border-danger border-2"
+          style={{ width: "5%" }}
+        ></div>
+        <div
+          className="border  border-danger border-2"
+          style={{ width: "1%" }}
+        ></div>
 
-export default Experince
+        <h3 className="fs-5 text-dark text-muted">Experience</h3>
+      </div>
+      <div className=" mt-2 p-2 border border-danger "></div>
+    </div>
+  );
+};
+
+export default Experince;
